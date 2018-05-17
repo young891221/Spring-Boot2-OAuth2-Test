71b54ceb5b33e7e8da09743ea769b740793b3b85
young891221 <young891221@gmail.com>
2018-05-17 16:55:49 +0900

    필요없는 과정 제거

--- src/main/java/com/example/boot2oauth/LoginController.java
+++ src/main/java/com/example/boot2oauth/LoginController.java
@@ -1,19 +1,8 @@
 package com.example.boot2oauth;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by KimYJ on 2018-04-12.
@@ -21,31 +10,8 @@ import java.util.Map;
 @RestController
 public class LoginController {
 
-    private OAuth2AuthorizedClientService authorizedClientService;
-
-    public LoginController(OAuth2AuthorizedClientService authorizedClientService) {
-        this.authorizedClientService = authorizedClientService;
-    }
-
     @GetMapping("/loginSuccess")
     public Object getLoginInfo(OAuth2AuthenticationToken authentication) {
-        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
-
-        String userInfoEndpointUri = client.getClientRegistration()
-                .getProviderDetails()
-                .getUserInfoEndpoint()
-                .getUri();
-
-        Map userAttributes = new HashMap();
-
-        if (!StringUtils.isEmpty(userInfoEndpointUri)) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken().getTokenValue());
-
-            ResponseEntity<Map> response = new RestTemplate().exchange(userInfoEndpointUri, HttpMethod.GET, new HttpEntity<>("", headers), Map.class);
-            userAttributes = response.getBody();
-        }
-
-        return userAttributes;
+        return authentication.getPrincipal().getAttributes();
     }
 }
